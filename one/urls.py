be8fae3a33fc73aaed78e6c11ecc56a96f6b9ca1
manish <manish21.12.15@gmail.com>
2020-01-04 18:11:54 +0530

    repotschange

--- one/urls.py
+++ one/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
   path('neworder/',views.new_order,name = 'New-Order-with-manish'),
   path('checkonlineorder/',views.check_online_order,name ='Check-Online-Order'),
   path('tracking/',views.tracking,name='Tracking_is_changed')SSSSSSS,
-  path('reports/',views.reports,name='Reports'),
+  path('reports/',views.reports,name='Reports_is_back')S,
   path('counter/',views.counter,name='Counter'),
   
 
