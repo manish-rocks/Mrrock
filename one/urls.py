72fd241e0413eee08a795bc1e978993d98206673
manish <manish21.12.15@gmail.com>
2020-01-04 18:01:43 +0530

    changed name

--- one/urls.py
+++ one/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
   #this byme
   path('dashboard/',views.dashboard,name = 'Dashboard'),
   path('applicationsetup/',views.application_setup,name = 'Application-Setup'),
-  path('neworder/',views.new_order,name = 'New-Order'),
+  path('neworder/',views.new_order,name = 'New-Order-with-manish'),
   path('checkonlineorder/',views.check_online_order,name ='Check-Online-Order'),
   path('tracking/',views.tracking,name='Tracking'),
   path('reports/',views.reports,name='Reports'),
